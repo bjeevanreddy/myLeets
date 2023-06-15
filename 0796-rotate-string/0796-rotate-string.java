664d4f92dd932fb84b09ba6da40991526fe61cd2
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-06-15 13:45:14 -0500

    Time: 1 ms (37.36%), Space: 40.9 MB (35.91%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean rotateString(String X, String Y) {
+        
+        // one test case failed
+        
+        // for X="abcde"  Y="abced" 
+//         if(X=="" || Y==""){
+// 			return false;
+// 		}
+// 		if(X.length()!=Y.length()){
+// 			return false;
+// 		}
+// 		int[] count = new int[256];
+// 		for(int i=0;i<X.length();i++){
+// 			 count[X.charAt(i)]++;
+// 			 count[Y.charAt(i)]--;
+// 		}
+// 		for(int i:count){
+// 			if(i>0){
+// 				return false;
+// 			}else{
+                
+//             }
+// 		}
+		
+// 		return true;
+//     }
+    
+    
+    ///normal method
+    int x= X.length();
+    int y=Y.length();
+    
+    int i=0;
+    while(i<x){
+        X=X.substring(1)+X.substring(0,1);
+        if(X.equals(Y)){
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+}
