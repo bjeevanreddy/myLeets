2f8677d450dc13e15517065b6e40d780b4bb311f
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-06-21 03:11:03 -0400

    Time: 1 ms (81.22%), Space: 44.6 MB (48.54%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode removeElements(ListNode head, int val) {
+    public ListNode removeElements1(ListNode head, int val) {
         
         // ListNode h=head;
         if(head==null){
@@ -22,12 +22,39 @@ class Solution {
         
         while(h.next != null){
             if(h.next.val==val){
-                // ListNode prev= h;
-                h.next=h.next.next;
-                // prev.next=n;    
+                h.next=h.next.next;   
             }else h=h.next;
         }
         
         return p.next;
     }
+    
+     public ListNode removeElements(ListNode head, int val) {
+        
+        // ListNode h=head;
+        if(head==null){
+            return null;
+        }
+        
+        ListNode result=new ListNode();
+        result.next=head;
+         ListNode h= head;
+        ListNode prev=result;
+         
+        
+        while(h != null){
+            if(h.val==val){
+                prev.next=h.next;
+            }else{
+                prev=h;
+               
+            }
+            //     h.next=h.next.next;   
+            // }else h=h.next;
+             h=h.next;
+        }
+        
+        return result.next;
+    }
+    
 }
