d6bdc725a8fa418653b6097bce3de8e795bd7a61
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-07-09 20:37:32 -0400

    Time: 21 ms (62.85%), Space: 57.1 MB (18.52%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -68,7 +68,7 @@ class Solution {
              return false;
          }
          HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
+    for(int i = 0; i < nums.length; i++){
         if(map.containsKey(nums[i])){
             if(Math.abs(i - map.get(nums[i])) <= k ){
                 return true;
