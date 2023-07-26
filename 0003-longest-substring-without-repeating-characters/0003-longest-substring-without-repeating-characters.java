23f30106135149b71c2b0ea563ab1146f9c7d042
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-07-26 01:08:35 -0500

    Time: 7 ms (73.85%), Space: 43.8 MB (48.07%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int lengthOfLongestSubstring1(String s) {
+
+        Set<Character> set= new HashSet<>();
+        int len = 0;
+        int start=0;
+        int right =0;
+        if(s.length()==1 ){
+            return 1;
+        }
+        if(s== "" || s==null){
+            return 0;
+        }
+        char[] chars = s.toCharArray();
+        // for(char c=0;c<chars.length;c++){
+        //     if(!set.contains(chars[c])){
+        //         set.add(chars[c]);
+        //         len = Math.max(len,set.size());
+        //     }else{
+        //         set.remove(chars[start]);
+        //         start++;
+        //         // set.add(chars[c]); 
+        //         c--;   
+        //    }
+          
+        // }
+         for (int e = 0; e < s.length(); e++) {
+            while(!set.add(s.charAt(e))) {
+                set.remove(s.charAt(start++));
+            }
+            len = Math.max(len, e - start + 1);
+        }
+        return len;
+
+        
+    }
+
+//     while(right<s.length()){
+//         if(set.contains(chars[right])){
+//             set.remove(chars[start]);
+//             start++;
+//         }
+//         else{
+//             set.add(chars[right++]);
+//             len = Math.max(len,set.size());
+//         }
+//     }
+//     return len;
+// }
+    
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        
+        int b=0;
+        int e=0;
+        int max =Integer.MIN_VALUE;
+        if(s==""|| s==null){
+            return 0;
+        }
+        int n = s.length();
+        while(e<n){
+            while(e<n && set.add(s.charAt(e))){
+                max= Math.max(max, e-b+1);
+                e++;
+            }
+            set.remove(s.charAt(b));
+            b++;
+        }
+        return max==Integer.MIN_VALUE ?0:max;
+    }
+}
