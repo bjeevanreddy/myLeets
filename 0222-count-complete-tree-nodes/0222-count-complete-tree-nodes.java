2956de11414ab01d44b5f9992d0a007b2fb6a893
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-08-10 10:45:51 -0500

    Time: 0 ms (100.00%), Space: 46.3 MB (11.56%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -19,9 +19,9 @@ class Solution {
             return 0;
         }
         
-//         if(root.left==null || root.right==null){
-//             return 1;
-//         }
+        if(root.left==null && root.right==null){
+            return 1;
+        }
         
         
         return 1 + countNodes(root.left) + countNodes(root.right);
