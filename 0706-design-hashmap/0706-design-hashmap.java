a4c2c14b7d66fff04e403965ae0858205fe935a6
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-10-03 23:42:18 -0500

    Time: 247 ms (11.16%), Space: 48.7 MB (58.20%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,6 +1,6 @@
-class MyHashMap {
+class MyHashMap1 {
    int[] arr;
-    public MyHashMap() {
+    public MyHashMap1() {
         arr =new int[1000001];
         Arrays.fill(arr,-1);
     }
@@ -15,6 +15,43 @@ class MyHashMap {
     
     public void remove(int key) {
         arr[key]=-1;
+    }
+}
+
+
+class MyHashMap {
+    ArrayList<ArrayList<Integer>> list;
+    public MyHashMap() {
+       list = new ArrayList<ArrayList<Integer>>();
+    }
+    
+    public void put(int key, int value) {
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).get(0)==key){
+               list.remove(i);
+            }
+        }
+        ArrayList<Integer> listN = new ArrayList<Integer>();
+        listN.add(key);
+        listN.add(value);
+        list.add(listN);
+    }
+    
+    public int get(int key) {
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).get(0)==key){
+                return list.get(i).get(1);
+            }
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).get(0)==key){
+               list.remove(i);
+            }
+        }
     }
 }
 
