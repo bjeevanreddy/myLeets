dfe4163f0ad3fa365bd5a21698677d646858079c
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-10-05 19:23:11 -0500

    Time: 21 ms (5.90%), Space: 46.1 MB (84.67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement1(int[] nums) {
         List<Integer> result = new ArrayList<>();
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i:nums){
@@ -13,9 +13,26 @@ class Solution {
                 result.add(x.getKey());
             }
         }
-        
-        return result;
-        
-        
+        return result;   
+    }
+    
+     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        double macVal =  Math.floor(nums.length/3);
+        if(nums.length==1){
+             result.add(nums[0]);
+             return result;
+         } 
+        for(int i:nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+            if(map.containsKey(i) && map.get(i)!=-1){
+                if(map.get(i)>macVal){
+                    map.put(i,-1);
+                    result.add(i);
+                }
+            }
+        }
+        return result;   
     }
 }
