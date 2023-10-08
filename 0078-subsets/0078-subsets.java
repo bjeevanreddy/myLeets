31333debf4f6007349ae688dda5ef7a1a7a98cda
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-10-07 23:24:39 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (13.81%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
          List<List<Integer>> reuslt =  new ArrayList<List<Integer>>();
-          helper(nums,0,reuslt,new ArrayList<>());
+          subSeq(nums,reuslt,0,new ArrayList<>());
           return reuslt;
     }
 
@@ -13,5 +13,16 @@ class Solution {
             helper(arr,i+1,res,curr);
             curr.remove(curr.size()-1);
         }
+    }
+    
+    public static void subSeq(int[] nums, List<List<Integer>> res, int start, List<Integer> temp){
+        if(start>=nums.length){
+              res.add(new ArrayList<>(temp)); 
+              return;
+            }
+        temp.add(nums[start]);
+        subSeq(nums, res, start+1, temp);
+        temp.remove(temp.size()-1);
+        subSeq(nums, res,start+1, temp);
     }
 }
