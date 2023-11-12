31f2c6aa76b5237578f2d0902ca3ebe61454180f
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-11-11 22:05:07 -0600

    Time: 1 ms (76.68%), Space: 39.2 MB (77.87%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -3,15 +3,14 @@ class Solution {
         if(n==1){
             return true;
         }
-        while(n>0){
-            
-            if(n%4!=0) return false;
-            
-            n=n/4;
-            
-            if(n==1) return true;
-        }
+        if(n==0) return false;
         
-        return false;
+        return n%4==0 && isPowerOfFour(n/4);
+        // while(n>0){
+        //     if(n%4!=0) return false;
+        //     n=n/4;
+        //     if(n==1) return true;
+        // }
+        // return false;
     }
 }
