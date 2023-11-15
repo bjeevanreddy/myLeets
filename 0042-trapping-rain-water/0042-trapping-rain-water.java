31bac0f888ec8660e783a39b01cdbf59ae39a976
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-11-14 21:32:59 -0600

    Time: 1 ms (75.82%), Space: 44.1 MB (70.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int trap(int[] height) {
+    
+    // BruteFrce- n^2
+    public int trap1(int[] height) {
         int l = height.length;
         int sum =0;
         for(int i=1;i<=l-2;i++){
@@ -18,6 +20,29 @@ class Solution {
             
             int diff = Math.min(lb,rb);
             sum += diff - (height[i]);
+        }
+        return sum;
+    }
+    
+    // 2 arrays - O(n) and space is n
+    public int trap(int[] height){
+        int sum = 0;
+        int l = height.length;
+        
+        int[] lb = new int[l];
+        int[] rb = new int[l];
+        
+        lb[0] = height[0];
+        rb[l-1] = height[l-1];
+        for(int i=1;i<l;i++){
+           lb[i] = Math.max(height[i], lb[i-1]); 
+        }
+        for(int j=l-2;j>=0;j--){
+           rb[j] = Math.max(height[j], rb[j+1]); 
+        }
+        
+        for(int k=0;k<l;k++){
+            sum += Math.min(lb[k], rb[k]) - height[k];
         }
         return sum;
     }
