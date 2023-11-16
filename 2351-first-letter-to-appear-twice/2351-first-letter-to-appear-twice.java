d6fec490f3e85f80ad706730db186ce038240e8f
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2023-11-15 18:52:26 -0600

    Time: 0 ms (100.00%), Space: 40.7 MB (11.81%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
+                return c;
+            }else{
+                map.put(c, c);
+            }
+        }
+        return new Character('0');
+    }
+}
