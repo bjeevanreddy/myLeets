5b9819fcde1d1ea51129afe04ccd04f6621d572e
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-06 22:45:47 -0500

    Time: 6 ms (82.57%), Space: 47.5 MB (32.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -18,25 +18,20 @@ class Solution {
         }
         return true;
     }
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] strs) {
         List<List<String>> result = new ArrayList<>();
         for(int i=0;i<strs.length;i++){
             String a = strs[i];
             boolean skip = false;
-            for(List<String> r:result){
-                if(r.contains(a)){
-                    skip = true;
-                    break;
-                }
-            }
-            if(skip){
+             var y = result.stream().flatMap(li -> li.stream()).collect(Collectors.toList());
+            if(y.contains(a)){
                 continue;
             }
             List<String> temp = new ArrayList<>();
             temp.add(a);
             for(int j=i+1;j<strs.length;j++){
                 String b = strs[j];
-                if( findA( a, b)){
+                if(findA( a, b)){
                     temp.add(b);
                 }
             }
@@ -44,4 +39,53 @@ class Solution {
         }
        return result;
     }
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs){
+            String key = buildKey(str);
+            map.putIfAbsent(key, new LinkedList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    private String buildKey(String s){
+        char[] freq = new char[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+        return new String (freq);
+    }
+    
+    
+    //pratcise
+     public List<List<String>> groupAnagrams(String[] strs){
+         Map<String, List<String>> result = new HashMap<>();
+         
+         for(String s: strs){
+             String key = subElement(s);
+             
+             if(result.containsKey(key)){
+                 List<String> x = result.get(key);
+                 x.add(s);
+                 result.put(key, x);
+             }
+             else{
+                 List<String> x= new ArrayList<>();
+                 x.add(s);
+                 result.put(key, x);
+             }
+         }
+         
+        return new ArrayList<>(result.values());
+     }
+    
+    private String subElement(String s){
+        char[] y = s.toCharArray();
+        Arrays.sort(y);
+        return new String(y);
+    }
+    
+    
+    
 }
