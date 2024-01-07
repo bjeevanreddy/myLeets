9f316a8c8dcc8a94fd6b5f48e367ab07b7fdfdc8
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-07 10:15:21 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (7.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -26,7 +26,7 @@ class Solution {
         return dp[m][n] = helper(m,n-1,dp) + helper(m-1,n,dp);
     }
     
-    public int uniquePaths(int m, int n) {
+    public int uniquePaths2(int m, int n) {
         
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
@@ -44,4 +44,36 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
+    
+    
+    // pratcise
+     public int uniquePaths(int m, int n) {
+         int s=0; int e=0;
+         
+         if(m==0 && n==0) return 0;
+         int dp[][] = new int[m][n];
+         for(int[] r:dp){
+             Arrays.fill(r, -1);
+         }
+         return helper2(m, n, s, e,dp);
+         
+     }
+    
+    
+    int helper2(int m, int n, int s, int e, int[][] dp){
+        
+        if(s>=m || e>=n) return 0;
+        
+        if(s==m-1 || e==n-1) return 1; 
+        
+        if(dp[s][e]!=-1){
+            return dp[s][e];
+        }
+        
+        int right = helper2(m, n, s+1, e, dp);
+        int left = helper2(m, n, s, e+1, dp);
+        
+        return dp[s][e]=right+left;
+    }
+    
 }
