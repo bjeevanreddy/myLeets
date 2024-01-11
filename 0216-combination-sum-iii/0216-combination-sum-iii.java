69faebd47cbcbab22a9b14a75a2ea810e3a77ef8
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-11 00:13:22 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (41.67%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -3,9 +3,9 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         
         List<Integer> narr = new ArrayList<>();
-        for(int i=0;i<9;i++){
-            narr.add(i+1);
-        }
+        // for(int i=0;i<9;i++){
+        //     narr.add(i+1);
+        // }
         
         if(n<=45) {
             helper(narr, k, n, 0,0, new ArrayList<>());
@@ -28,8 +28,8 @@ class Solution {
          if((temp.size()<k && sum>n)) return;
         
         if(start>8) return;
-        temp.add(narr.get(start));
-        helper(narr, k, n, start+1, sum+narr.get(start), temp);
+        temp.add(start+1);
+        helper(narr, k, n, start+1, sum+start+1, temp);
         temp.remove(temp.size()-1);
         helper(narr, k, n, start+1, sum, temp);
     }
