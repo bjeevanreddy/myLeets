ac475442dad66b170d27c6c79dd4307b21b06134
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-10 23:24:32 -0500

    Time: 2 ms (80.56%), Space: 44.7 MB (13.95%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,11 +1,11 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum1(int[] candidates, int target) {
     List<List<Integer>> res = new ArrayList<>();
-     helper(candidates, 0, target, res, new ArrayList<>());
+     helper1(candidates, 0, target, res, new ArrayList<>());
         return res;
     }
    
-    void helper(int[] candidates, int start, int t,  List<List<Integer>> res, List<Integer> temp){
+    void helper1(int[] candidates, int start, int t,  List<List<Integer>> res, List<Integer> temp){
          
          if(start>=candidates.length || t<0){
              return;
@@ -16,10 +16,39 @@ class Solution {
              return;
          }
         temp.add(candidates[start]);
-        helper(candidates, start, t-candidates[start],res, temp);
+        helper1(candidates, start, t-candidates[start],res, temp);
         temp.remove(temp.size()-1);
-        helper(candidates, start+1, t ,res, temp);  
+        helper1(candidates, start+1, t ,res, temp);  
        
      }
+    
+    
+    //Pratcise
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(target==0) return null;
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, 0, new ArrayList<>(),target, result);
+        return result;
+     }
+    
+    static void helper(int[] cn, int start, List<Integer> temp, int target, List<List<Integer>> result){
+        
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        if (start>=cn.length) return;
+        
+        if(target<0) return;
+        
+        temp.add(cn[start]);
+        helper(cn, start, temp, target-cn[start], result);
+         temp.remove(temp.size()-1);
+        helper(cn, start+1, temp, target, result);
+    }
+    
+    
+    
     
 }
