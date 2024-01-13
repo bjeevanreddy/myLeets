5a4f134bddf793efa224e0394076502f27e3e66b
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-12 23:21:56 -0500

    Time: 0 ms (100.00%), Space: 41.4 MB (5.70%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -48,7 +48,7 @@ class Solution {
     }
     
      //recursion + memo
-    public int rob(int[] nums) {
+    public int rob4(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
         return recu(0, nums, dp);
@@ -59,6 +59,23 @@ class Solution {
             return dp[s];
         }
         return dp[s] = Math.max(nums[s] + recu(s+2, nums,dp) ,recu(s+1, nums,dp));
+    }
+    
+     //bottom up 
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        if(nums.length==1){
+            return nums[0];
+        }
+        if(nums.length==2) return Math.max(nums[0], nums[1]);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i] = Math.max(dp[i-1] ,nums[i] + dp[i-2]);
+            // System.out.println(dp[i]);
+        }
+        
+        return dp[nums.length-1];
     }
         
         
