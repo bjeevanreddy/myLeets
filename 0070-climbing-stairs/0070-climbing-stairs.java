197c59dd66386ee7b218a19afa3d8b0bb75c8976
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-12 22:52:08 -0500

    Time: 0 ms (100.00%), Space: 40.1 MB (40.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    
+    // tabulation- bottom-up
     public int climbStairs1(int n) {
         
       
@@ -20,7 +20,9 @@ class Solution {
         
     }
     
-     public int climbStairs(int n) {
+    
+    // recursion + memoization -- top-down
+     public int climbStairs2(int n) {
         
       int dp[] = new int[n+1];
     
@@ -43,4 +45,33 @@ class Solution {
          return dp[n] = helper(n-1,dp)+helper(n-2,dp);
         
     }
+    
+     public int climbStairs(int n){
+         
+         int[] dp = new int[n+1];
+         dp[0] = 1;
+         dp[1] = 1;
+         for(int i=2;i<=n;i++){
+             dp[i] = dp[i-1]+dp[i-2];
+         }
+        return dp[n];
+     }
+    
+    int recu(int start, int n){
+        if(start==n) {
+            return 1;
+        }
+        if(start>n) return 0;
+        return recu(start+1,n) + recu(start+2, n);
+    }
+    
+    int recu1(int n){
+        if(n==0) {
+            return 1;
+        }
+        if(n<0) return 0;
+        return recu1(n-1) + recu1(n-2);
+    }
+    
+    
 }
