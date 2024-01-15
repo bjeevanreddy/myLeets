33ee9a68798de3586101bc058cef0c12ad23fa09
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-14 23:52:05 -0500

    Time: 0 ms (100.00%), Space: 44.8 MB (13.36%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,21 +14,35 @@
  * }
  */
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        
-        List<Integer> l = new ArrayList<>();
-        
-        helper(root, l);
-        System.out.println(l);
-        return l.get(k-1);
+    // with extra space
+//     public int kthSmallest(TreeNode root, int k) {
+//         List<Integer> l = new ArrayList<>();
+//         helper(root, l);
+//         return l.get(k-1);
+//     }
+    
+//     void helper(TreeNode r, List<Integer> l){ 
+//         if(r==null) return;
+//         helper(r.left, l);
+//         l.add(r.val);
+//         helper(r.right, l);
+//     }
+    
+    int c=0;
+    int x=0;
+     public int kthSmallest(TreeNode root, int k) {
+        helper(root,k);
+        return x;
     }
     
-    void helper(TreeNode r, List<Integer> l){
-        
+    void helper(TreeNode r, int k){ 
         if(r==null) return;
-       
-        helper(r.left, l);
-        l.add(r.val);
-        helper(r.right, l);
+        helper(r.left, k);
+        c++;
+        if(k == c){
+            x = r.val;
+            return;
+        }
+        helper(r.right, k);
     }
 }
