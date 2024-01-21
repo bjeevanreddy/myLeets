4148543bb77f2e470c990e3bf8d6b548ffae11a0
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-21 16:11:02 -0500

    Time: 63 ms (31.50%), Space: 54.5 MB (51.50%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -1,5 +1,7 @@
 class Solution {
-    public String largestWordCount(String[] messages, String[] senders) {
+    
+    //using priorty queuue
+    public String largestWordCount1(String[] messages, String[] senders) {
         
         int n= messages.length;
         Map<String,Integer> map = new HashMap<>();
@@ -20,5 +22,29 @@ class Solution {
         }
         
         return x.poll();
+    }
+    
+    // only hashMap
+    public String largestWordCount(String[] messages, String[] senders) {
+        HashMap<String,Integer> hm=new HashMap<>();
+		
+        int max=0;
+        String name="";
+        for(int i=0;i<messages.length;i++){
+            String[] words=messages[i].split(" ");
+            
+            int freq=hm.getOrDefault(senders[i],0)+words.length;
+            hm.put(senders[i],freq);
+            
+            if(hm.get(senders[i])>max){
+                max=hm.get(senders[i]);
+                name=senders[i];
+            }
+            else if(hm.get(senders[i])==max && name.compareTo(senders[i])<0){
+                name=senders[i];
+            } 
+        }
+        
+        return name;
     }
 }
