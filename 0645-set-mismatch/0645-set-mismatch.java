02a5c4cd892ad7d4844b9212475f503d2653aa0d
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-21 21:08:29 -0500

    Time: 2 ms (97.74%), Space: 46.1 MB (8.03%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -4,23 +4,24 @@ class Solution {
         int[] x = new int[n+1];
         int missed=0;
         int dup=-1;
-       
+       int sum=0;
         for(int i=0;i<n;i++){
-           
+            sum+=nums[i];
             if(x[nums[i]]!=0){
                 dup = nums[i];
-                // break;
                 continue;
             }
-             x[nums[i]]=1;
-            
+             x[nums[i]]=1;  
         }
-         for(int i=1;i<=n;i++){
-            if(x[i]==0){
-                missed = i;
-                break;
-            }     
-        }
+        
+        int actual = n *(n+1)/2;
+        missed = (actual-sum)+dup;
+        //  for(int i=1;i<=n;i++){
+        //     if(x[i]==0){
+        //         missed = i;
+        //         break;
+        //     }     
+        // }
         return new int[]{dup,missed};
     }
 }
