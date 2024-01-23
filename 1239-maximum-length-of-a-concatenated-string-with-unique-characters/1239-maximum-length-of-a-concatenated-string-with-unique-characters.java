af35bf763ab1cee49f34e9a7facf0ad41cc0718b
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-22 22:03:09 -0500

    Time: 13 ms (65.22%), Space: 44.8 MB (33.47%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int maxLength(List<String> arr) {
+        return solve(arr, 0, "");
+    }
+    
+    int solve(List<String> arr, int start, String temp){
+        
+        if(start>=arr.size()){
+            if(!isGood(temp)){
+                return 0;
+            }
+            return temp.length();
+        }
+        int pick = 0;
+        int notpick =0;
+        int test =0;
+        if(contains(temp, arr.get(start))){
+            pick = solve(arr, start+1, temp+arr.get(start));
+        }
+        notpick =  solve(arr, start+1, temp);
+        // else{
+        //     test= solve(arr, start+1, temp+arr.get(start));
+        // }
+        return Math.max(pick,notpick);
+    }
+    
+    boolean contains(String one, String two){
+        // if(one=="" || two == ""){
+        //     return true;
+        // }
+        int[] x= new int[26];
+        
+        for(char c:one.toCharArray()){
+            x[c-'a']++;
+            // if(x[c-'a']>1){
+            //     return false;
+            // }
+        }
+        for(char c:two.toCharArray()){
+            if(x[c-'a']!=0){
+                return false;
+            }
+            // x[c-'a']++;
+            // if(x[c-'a']>1){
+            //     return false;
+            // }
+        }
+        return true;
+    }
+     boolean isGood(String one){
+          int[] x= new int[26];
+        
+        for(char c:one.toCharArray()){
+            x[c-'a']++;
+            if(x[c-'a']>1){
+                return false;
+            }
+        }
+         return true;
+     }
+}
