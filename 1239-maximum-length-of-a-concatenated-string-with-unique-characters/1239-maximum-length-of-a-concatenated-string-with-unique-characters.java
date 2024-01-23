67b51ea495ebc7fe346bc0e75520d4b07f4efc59
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-22 22:26:35 -0500

    Time: 166 ms (9.58%), Space: 45.2 MB (22.58%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -4,52 +4,38 @@ class Solution {
     }
     
     int solve(List<String> arr, int start, String temp){
-        
         if(start>=arr.size()){
             if(!isGood(temp)){
                 return 0;
             }
             return temp.length();
         }
+        
+       
         int pick = 0;
         int notpick =0;
-        int test =0;
-        if(contains(temp, arr.get(start))){
+        // if(contains(temp, arr.get(start))){
             pick = solve(arr, start+1, temp+arr.get(start));
-        }
-        notpick =  solve(arr, start+1, temp);
-        // else{
-        //     test= solve(arr, start+1, temp+arr.get(start));
         // }
+        notpick =  solve(arr, start+1, temp);
         return Math.max(pick,notpick);
     }
     
     boolean contains(String one, String two){
-        // if(one=="" || two == ""){
-        //     return true;
-        // }
         int[] x= new int[26];
         
         for(char c:one.toCharArray()){
             x[c-'a']++;
-            // if(x[c-'a']>1){
-            //     return false;
-            // }
         }
         for(char c:two.toCharArray()){
             if(x[c-'a']!=0){
                 return false;
             }
-            // x[c-'a']++;
-            // if(x[c-'a']>1){
-            //     return false;
-            // }
         }
         return true;
     }
-     boolean isGood(String one){
-          int[] x= new int[26];
-        
+    boolean isGood(String one){
+        int[] x= new int[26];
         for(char c:one.toCharArray()){
             x[c-'a']++;
             if(x[c-'a']>1){
