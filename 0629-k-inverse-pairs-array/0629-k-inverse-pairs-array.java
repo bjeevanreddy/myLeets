9925d4ffd011c4715b0d0143b856dbdcc5ff875f
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-27 13:46:20 -0500

    Time: 1147 ms (30.25%), Space: 60.2 MB (15.13%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -1,7 +1,7 @@
 class Solution {
       int mod = 1000000007;
         Integer [] [] memo = new Integer [1001] [1001];
-    public int kInversePairs(int n, int k) {
+    public int kInversePairs1(int n, int k) {
         if (n == 0) {
             return 0;
         }
@@ -17,4 +17,21 @@ class Solution {
         }
         return memo [n] [k] = inversePairs;
     }
+    
+    public int kInversePairs(int n, int k){
+        int[] [] dp = new int [n + 1] [k + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                if (j == 0) {
+                    dp [i][j] = 1;
+                    }
+                else {
+                    for (int p = 0; p <= Math.min(j, i-1); p++) {
+                        dp [i][j] = (dp[i][j] + dp[i-1] [j-p]) % mod;
+                    }
+                }
+            }
+        }
+        return dp [n] [k];
+        }
 }
