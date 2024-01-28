46667e451ec186af9a8bd8273aa0af44f6484d73
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-28 12:30:37 -0500

    Time: 4 ms (55.02%), Space: 43.9 MB (35.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    
+    // Brutforce
+    public int[] nextGreaterElement1(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
        
@@ -19,7 +21,28 @@ class Solution {
             }
             if(flag) nums1[i]=-1;
         }
-        // if(flag) nums1[m-1]=-1;
         return nums1;
     }
+    
+   public int[] nextGreaterElement(int[] nums1, int[] nums2){
+       
+       HashMap<Integer, Integer> map = new HashMap<>();
+       
+       Stack<Integer> stack = new Stack<>();
+       
+       for(int i=0;i<nums2.length;i++){
+           
+           while(!stack.isEmpty() && stack.peek() < nums2[i]){
+               map.put(stack.pop(), nums2[i]);
+           }
+           
+           stack.add(nums2[i]);
+       }
+       int k=0;
+       for(int num:nums1){
+           nums1[k++] = map.getOrDefault(num, -1);
+       }
+       return nums1;
+   }
 }
+
