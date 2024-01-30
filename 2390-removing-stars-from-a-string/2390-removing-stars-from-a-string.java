feebe1b5840312828813dee4e2985ec6b27b69c0
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-29 22:54:54 -0500

    Time: 482 ms (21.26%), Space: 47.8 MB (24.59%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String removeStars(String s) {
+    public String removeStars1(String s) {
         String rsult="";
         Stack<Character> x = new Stack<>();
         
@@ -16,5 +16,16 @@ class Solution {
             rsult = x.pop() + rsult;
         }
         return rsult;
+    }
+    
+     public String removeStars(String s) {
+        String ans="";
+        int cntStar=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='*') cntStar++;
+            else if(cntStar==0) ans=s.charAt(i)+ans;
+            else cntStar--;
+        }
+        return ans;
     }
 }
