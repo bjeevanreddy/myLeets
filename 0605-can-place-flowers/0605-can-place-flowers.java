b96a75bfd15b11c9c01471d2fa0d45d63c7279ca
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-31 22:29:43 -0500

    Time: 1 ms (97.62%), Space: 45.3 MB (52.45%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        
+int count = 0;
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length;) {
+            
+            if (flowerbed[i] == 1) {
+                i = i + 2;
+            } else if (flowerbed[i] == 0 && i == flowerbed.length - 1) {
+                flowerbed[i] = 1;
+                count++;
+                i = i + 2;
+            } else if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                count++;
+                i = i + 2;
+            } else {
+                i++;
+            }
+            if (n == count) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+       
+}
+
+// 0 0 0 0
