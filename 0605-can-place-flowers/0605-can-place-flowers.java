22c8a546563dd7c86bc544d4e8210cb4f7ab5838
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-01-31 22:51:23 -0500

    Time: 1 ms (97.62%), Space: 45.9 MB (5.31%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -27,6 +27,35 @@ int count = 0;
         
         return false;
     }
+    
+    
+    // issue with edge cases
+    public boolean canPlaceFlowers1(int[] flowerbed, int n) {
+        
+        int k=-1;
+        for(int i=0;i<flowerbed.length;i++){
+            if(flowerbed[i]==1){
+                k=i;
+                continue;
+            }else if(i==0 && flowerbed[i]==0 && flowerbed[i+1]==0){
+                flowerbed[i]=1;
+                k=i;
+                n--;
+                continue;
+            }
+            else if(i-k >= 2){
+                k = i;
+                if(i+1<n &&flowerbed[i+1]==1){
+                    continue;
+                }
+                n--;
+            }
+            if(n==0){
+                return true;
+            }
+        }
+        return false;
+    }
        
 }
 
