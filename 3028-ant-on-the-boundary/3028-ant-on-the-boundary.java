5acbeaef4affee4c5e54dd2c4c2098a57646366d
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-02-04 15:49:17 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (66.67%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int count=0;int sum=0;
+        for(int i :nums){
+            sum+=i;
+            if(sum==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
