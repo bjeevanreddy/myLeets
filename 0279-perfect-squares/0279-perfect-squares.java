08de3bd8f55d8509a74d8d73fa7d6e5389bb6413
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-02-09 18:28:05 -0500

    Time: 65 ms (27.93%), Space: 43.3 MB (24.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numSquares(int n) {
+    public int numSquares1(int n) {
         int[] dp = new int[n + 1];
        Arrays.fill(dp, Integer.MAX_VALUE);
        dp[0] = 0;
@@ -10,4 +10,43 @@ class Solution {
        }
        return dp[n];
     }
+    
+    //recursion
+     int ans=0;
+     public int numSquares2(int n){
+        if(n<4){
+            return n;
+        }
+         ans=n;
+         for(int i=1;i*i<=n;i++){
+             int s = i*i;
+             ans = Math.min(ans, 1+numSquares(n-s));
+         }
+         
+         return ans;
+     }
+   
+    // Arrays.fill(dp,-1);
+    
+    public int numSquares(int n){
+         int[] dp = new int[n+1];
+        return helper(n, dp);
+    }
+    
+    int helper(int n, int[] dp){
+         if(n<4){
+            return n;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+         ans=n;
+         for(int i=1;i*i<=n;i++){
+             int s = i*i;
+             ans = Math.min(ans, 1+helper(n-s, dp));
+         }
+         
+         return dp[n]=ans;
+     }
+       
 }
