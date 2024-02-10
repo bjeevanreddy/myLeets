5b21e84a352966faedaedbb3aec8cb5cabd8f18b
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-02-09 19:38:25 -0500

    Time: 2 ms (96.29%), Space: 41.1 MB (86.32%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countSubstrings(String s) {
+    public int countSubstrings1(String s) {
       if(s.length() == 0)return 0;
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
@@ -27,5 +27,44 @@ class Solution {
             }
         }
         return res;
+    }
+    
+    public int countSubstrings2(String s) {
+    int n = s.length();
+    int res = 0;
+    boolean[][] dp = new boolean[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i  + 1 < 3 || dp[i + 1][j - 1]);
+            if(dp[i][j]) ++res;
+            System.out.print(dp[i][j]+" ");
+        }
+         System.out.println();
+    }
+    return res;
+}
+    
+    public int countSubstrings(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            c+=helper(s, i,i) + helper(s, i,i+1);
+        }
+        return c;
+           
+    }
+    
+    int helper(String s, int l, int e){
+        int c=0;
+        while(l>=0 && e<s.length() && l<=e){
+            if(s.charAt(l)==s.charAt(e)){
+                c++;
+                 l--;
+                e++;
+            }else{
+                break;
+            }
+           
+        }
+        return c;
     }
 }
