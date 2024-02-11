2c813b2d216fd91ef9b8125441c5eaabebc22fad
Jeevan <55045266+bjeevanreddy@users.noreply.github.com>
2024-02-11 13:46:30 -0500

    Time: 151 ms (53.78%), Space: 41.4 MB (62.17%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -7,7 +7,7 @@ class Solution {
         boolean[][] path = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               if( board[i][j]==word.charAt(0) && search(board, i,j,word, n,m, path)){
+               if( board[i][j]==word.charAt(0) && search(board, i,j,word,0, n,m, path)){
                    return true;
                }
             }
@@ -16,23 +16,21 @@ class Solution {
         return false;
     }
     
-    boolean search(char[][] board, int r, int c, String word,int n, int m, boolean[][] path){
-        if(word.isEmpty()){
+    boolean search(char[][] board, int r, int c, String word,int i,int n, int m, boolean[][] path){
+        if(i==word.length()){
             return true;
         }
         // System.out.println(word);
-        if(r>=n || c>=m || c<0 || r<0 || board[r][c]!=word.charAt(0) || path[r][c]){
+        if(r>=n || c>=m || c<0 || r<0 || board[r][c]!=word.charAt(i) || path[r][c]){
             return false;
         }
         
         path[r][c]=true;
         boolean x=false;boolean y=false;boolean z=false;
-        // for(int i=-1;i<=1;i++){
-         x= search(board, r, c+1, word.substring(1), n, m, path) ;
-          y =  search(board, r+1,c, word.substring(1), n, m,path) ;
-           z = search(board, r,c-1, word.substring(1), n, m, path);
-        boolean p = search(board, r-1,c, word.substring(1), n, m, path);
-        // }
+         x= search(board, r, c+1, word,i+1, n, m, path) ;
+          y =  search(board, r+1,c, word,i+1, n, m,path) ;
+           z = search(board, r,c-1, word,i+1 ,n, m, path);
+        boolean p = search(board, r-1,c, word,i+1, n, m, path);
         
         path[r][c]=false;
         return x||y||z||p;
